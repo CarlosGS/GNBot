0a1c2abe92448a792f6c03d6ebcfc7d433469e8f
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2013-08-14 11:32:26 -0700

    More representation tests

--- Software/LDR_process.py
+++ Software/LDR_process.py
@@ -11,6 +11,9 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+import scipy
+import scipy.interpolate
+
 from helper import *
 # End modules
 
@@ -19,6 +22,15 @@ LDR_DF_PATH = "./data/"
 # End configuration
 
 #plt.ion() # IMPORTANT: Enable real-time plotting
+
+def diff(vals):
+    #return np.diff(vals,axis=0)
+    #return np.abs(np.fft.fft(vals))
+    result = vals
+    for i in range(len(vals)):
+        result[i] = vals[i]-vals[i-1]
+    return result
+
 
 for LDR_DF in getFilesInDir(LDR_DF_PATH):
     print("\n=====================================")
@@ -94,8 +106,9 @@ for LDR_DF in getFilesInDir(LDR_DF_PATH):
     #plt.show()
     
     
+    # Polar plot
     fig = plt.figure()
-    ax = fig.add_subplot(111, polar=True)
+    ax = fig.add_subplot(111,polar=True)
     LDR_real_pos = np.array(LDR_data["LDR_real_pos"])
     for i in range(len(LDR_real_pos)):
         # NOTE: here we are incorporating the knowledge of the orientation of the sensors
@@ -103,11 +116,99 @@ for LDR_DF in getFilesInDir(LDR_DF_PATH):
         ax.plot(np.radians(MagAngle_raw+offset), LDR_raw[:,i], marker='.', markersize=2, ls='', label=str(i+1))
     
     for light in LDR_data["lights"]:
-        ax.plot(np.radians(light["angle"]), 1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
-    
-    ax.set_rmax(1050)
+        ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
+    #plt.ylim((0,2*1050))
+    ax.set_rmax(2*1050)
     ax.set_theta_direction('clockwise')
     ax.set_theta_zero_location('N')
+    ax.legend()
+    ax.grid(True)
+    
+    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
+    #mySaveFig(plt,LDR_DF_PATH + "png/","Nlights:" + str(Nlights) + " " + LDR_data["beginDate"] + "_2.png")
+    #plt.show()
+    
+    
+    
+    # Polar plot (fitted curve + points where derivate == 0)
+    fig = plt.figure()
+    ax = fig.add_subplot(111,polar=True)
+    
+    LDR_real_pos = np.array(LDR_data["LDR_real_pos"])
+    LDR_raw_all = np.array([])
+    LDR_raw_angle_all = np.array([])
+    for i in range(len(LDR_real_pos)):
+        # NOTE: here we are incorporating the knowledge of the orientation of the sensors
+        offset = float(LDR_real_pos[i])
+        
+        angle = np.radians(MagAngle_raw+offset)
+        angle = np.mod(angle, 2*np.pi);
+        magnitude = LDR_raw[:,i]
+        LDR_raw_all = np.hstack([LDR_raw_all,LDR_raw[:,i]])
+        LDR_raw_angle_all = np.hstack([LDR_raw_angle_all,angle])
+    
+    #indices = np.argsort(LDR_raw_angle_all,axis=None)
+    #LDR_raw_angle_all = LDR_raw_angle_all[indices]
+    #LDR_raw_all = LDR_raw_all[indices]
+    
+    #fit_pol = scipy.interpolate.splrep(LDR_raw_angle_all, LDR_raw_all, per=1)
+    fit_pol = np.poly1d(np.polyfit(LDR_raw_angle_all, LDR_raw_all, 6))
+    fit_angle = np.linspace(LDR_raw_angle_all.min(), LDR_raw_angle_all.max(), 800)
+    #fit_angle = np.linspace(0, 2*np.pi, 800)
+    fit_magnitude = fit_pol(fit_angle)
+    ax.plot(fit_angle, fit_magnitude, label="Fitted curve")
+    ax.plot(LDR_raw_angle_all, LDR_raw_all, marker='.', markersize=0.5, ls='', label="Datapoints")
+    
+    fit_pol_der = np.polyder(fit_pol)
+    fit_magnitude_der = fit_pol_der(fit_angle)
+    fit_magnitude_der_min = abs(np.min(fit_magnitude_der))
+    fit_magnitude_der += fit_magnitude_der_min
+    
+    # Find points with derivate == 0
+    all_roots = np.roots(fit_pol_der)
+    
+    # Remove complex roots
+    indices = np.where(np.imag(all_roots)==0)
+    roots = np.real(all_roots[indices])
+    
+    # Remove roots outside [0,2pi)
+    indices = (roots >= 0) & (roots < 2*np.pi)
+    roots = roots[indices]
+    print("Roots: "+str(roots))
+    
+    ax.plot(roots, fit_pol(roots), marker='x', markersize=10, ls='', label="Roots")
+    
+    for light in LDR_data["lights"]:
+        ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
+    #plt.ylim((0,2*1050))
+    ax.set_rmax(2*1050)
+    ax.set_theta_direction('clockwise')
+    ax.set_theta_zero_location('N')
+    #ax.legend()
+    ax.grid(True)
+    
+    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
+    #mySaveFig(plt,LDR_DF_PATH + "png/","Nlights:" + str(Nlights) + " " + LDR_data["beginDate"] + "_2.png")
+    #plt.show()
+    
+    
+    
+    # Linear plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    ax.plot(LDR_raw_angle_all, LDR_raw_all, marker='.', markersize=0.5, ls='', label="Datapoints")
+    ax.plot(fit_angle, fit_magnitude, label="Fitted curve")
+    
+    ax.plot(fit_angle, fit_magnitude_der, label="Fitted curve (diff)")
+    ax.plot(fit_angle, fit_magnitude_der_min + 0*fit_magnitude_der)
+    
+    ax.plot(roots, fit_pol(roots), marker='x', markersize=10, ls='', label="Roots")
+    
+    for light in LDR_data["lights"]:
+        ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
+    plt.ylim((0,2*1050))
+    
     ax.legend()
     ax.grid(True)
     
