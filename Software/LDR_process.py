f051bf71ec63d6af98320fda32c813c8560cec2b
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2013-08-27 11:16:56 +0200

    More tests with the light sensor

--- Software/LDR_process.py
+++ Software/LDR_process.py
@@ -11,9 +11,6 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-import scipy
-import scipy.interpolate
-
 from helper import *
 # End modules
 
@@ -21,16 +18,29 @@ from helper import *
 LDR_DF_PATH = "./data/"
 # End configuration
 
-#plt.ion() # IMPORTANT: Enable real-time plotting
+# Data from the measurement with one light and no ambient light
+Light1_LDR_curve = np.array([232.56363636363633, 235.3166666666666, 248.2173913043478, 259.352380952381, 276.0769230769231, 292.81000000000006, 305.46399999999994, 316.01599999999996, 338.83333333333337, 396.3083333333334, 495.16999999999996, 567.0560000000002, 613.44, 654.4173913043478, 685.4782608695652, 706.5826086956522, 720.7652173913043, 731.6260869565218, 740.1043478260871, 747.162962962963, 745.96, 747.0999999999999, 743.0380952380954, 739.9000000000001, 727.3636363636365, 719.4952380952379, 702.2166666666667, 678.904347826087, 651.1047619047619, 596.5923076923076, 552.2700000000001, 471.1461538461538, 388.39200000000005, 313.46666666666664, 274.425, 254.23999999999995, 247.07199999999995, 227.47000000000003, 210.89565217391302, 204.33043478260868, 193.295652173913, 185.50434782608693, 184.19130434782608, 187.96521739130438, 191.02222222222224, 193.92, 198.03846153846155, 204.21904761904764, 208.32500000000002, 217.4090909090909])
+Light1_angle = 144.73
+Light1_distance = 106.0
+Light1_intensity = 60.0
 
-def diff(vals):
-    #return np.diff(vals,axis=0)
-    #return np.abs(np.fft.fft(vals))
-    result = vals
-    for i in range(len(vals)):
-        result[i] = vals[i]-vals[i-1]
-    return result
+# Data from the measurement with two lights, no ambient light and removing the effect of the first light
+Light2_LDR_curve = np.array([596.897902097902, 603.4659420289854, 599.0608695652172, 592.361904761905, 580.7412587412585, 566.2780000000001, 553.7110000000002, 542.8080000000001, 516.7484848484846, 458.61166666666657, 358.87000000000023, 283.6856666666665, 231.40761904761916, 184.34782608695684, 144.30269151138725, 111.28405797101448, 79.26811594202911, 53.42608695652166, 38.747826086956366, 31.321652421652402, 33.22999999999979, 27.97407407407411, 28.088220551378413, 25.363999999999805, 30.584189723320037, 25.88737060041433, 26.21811594202893, 34.06086956521722, 29.885714285714243, 61.99860139860152, 60.40199999999993, 87.66217948717957, 110.62399999999991, 157.67878787878794, 174.79899999999992, 198.84000000000015, 208.19466666666676, 228.15857142857135, 250.4347826086956, 262.6505175983437, 283.54601449275367, 300.07065217391306, 318.0086956521739, 338.6695652173913, 391.7777777777777, 445.4200000000001, 508.46524216524233, 555.3809523809524, 578.851, 594.1996047430829])
+Light2_angle = 41.62
+Light2_distance = 63.0
+Light2_intensity = 60.0
 
+# Data from the measurement with three lights, no ambient light and removing the effect of the first and second lights
+Light3_LDR_curve = np.array([-1.3015384615382573, -5.018972332016006, -3.5502608695649087, -2.287012987013213, -2.887747035572829, -3.342545454545757, -1.490384615384869, -1.7360000000002174, 0.37272727272750217, 1.4163636363635987, 2.4183333333331802, 4.206159420289623, 5.670562770562469, 7.060869565217104, 9.152380952380781, 14.975000000000023, 23.1575757575755, 28.8144927536232, 31.909730848861386, 34.21538461538478, 36.07363636363664, 40.80774410774427, 41.691866028707864, 44.12830769230766, 44.49427917620153, 53.300724637681014, 60.7470355731225, 63.62678260869575, 78.94588744588748, 81.00039525691693, 104.90981818181808, 128.64551282051298, 155.18400000000008, 156.2636363636363, 162.69418181818185, 144.29500000000002, 122.15942028985495, 111.47142857142853, 94.2434782608697, 90.78095238095244, 81.39999999999986, 75.87954545454551, 68.11666666666673, 61.90807453416153, 40.24285714285736, 25.20545454545436, 10.632659932659976, -1.481818181818312, -2.037538461538702, -2.387643020594737])
+Light3_angle = 176.26
+Light3_distance = 95.0
+Light3_intensity = 30.0
+
+# Data from the measurement with four lights, no ambient light and removing the effect of the first, second and third lights
+Light4_LDR_curve = np.array([24.101538461538325, 23.49636363636398, 16.755333333333283, 16.67672727272725, 13.351383399209453, 14.236363636363876, 11.49938461538477, 11.75200000000018, 11.681818181818016, 10.943636363636415, 11.157051282051384, 10.191304347826303, 11.61225296442717, 11.873913043478183, 12.823188405797168, 12.732246376811645, 13.904329004329156, 19.637681159420367, 22.063095238095002, 21.48333333333312, 19.79723320158098, 20.68181818181813, 23.86007905138365, 25.299358974359052, 30.07607655502386, 31.664814814814918, 42.298181818181774, 55.87466666666671, 74.15163636363616, 95.1814229249012, 118.30000000000018, 147.6501538461539, 182.4240000000001, 210.7727272727273, 228.91181818181843, 242.62500000000006, 263.69565217391283, 274.6565217391304, 285.10942028985505, 281.48157349896474, 277.80181159420306, 269.2502164502164, 259.11811594202914, 236.6404761904762, 201.1821428571426, 161.72411067193684, 112.03636363636338, 76.54268774703553, 55.13653846153875, 34.84258373205728])
+Light4_angle = 263.02
+Light4_distance = 65.0
+Light4_intensity = 30.0
 
 for LDR_DF in getFilesInDir(LDR_DF_PATH):
     print("\n=====================================")
@@ -70,70 +80,65 @@ for LDR_DF in getFilesInDir(LDR_DF_PATH):
     MagAngle_raw = np.array(LDR_data["MagAngle_raw"])
     
     # Invert LDR sensor values from [0,1023] to [1023,0] (to have 0 -> dark, 1023 -> bright)
-    for i in range(len(LDR_raw[0,:])):
-        LDR_raw[:,i] = 1023.0-LDR_raw[:,i]
+    LDR_raw = 1023.0-LDR_raw
     
     # Normalize sensor values
     # NOTE: Here we are using all the values, the robot only has an incremental sequence
     #for i in range(len(LDR_raw[0,:])):
     #    LDR_raw[:,i] = mapVals(LDR_raw[:,i], LDR_raw[:,i].min(), LDR_raw[:,i].max(), 0.0, 1023.0)
     
-    
-    fig = plt.figure()
-    
-    ax = fig.add_subplot(311)        
-    ax.plot(time_raw,LDR_raw)
-    ax.grid(True)
-    plt.ylim((0,1050))
-    ax.legend(["1","2","3","4"])
-    plt.ylabel('Light intensity')
-    
-    ax = fig.add_subplot(312)        
-    ax.plot(time_raw,MagAngle_raw)
-    ax.grid(True)
-    plt.ylim((0,360))
-    plt.ylabel('Compass angle [Deg]')
-    
-    ax = fig.add_subplot(313)        
-    ax.plot(time_raw,BattVoltage_raw)
-    ax.grid(True)
-    plt.ylim((5,10))
-    plt.ylabel('Battery voltage [V]')
-    plt.xlabel('Time [ms]')
-    
-    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
+    # Plot the raw data
+#    fig = plt.figure()
+#    
+#    ax = fig.add_subplot(311)        
+#    ax.plot(time_raw,LDR_raw)
+#    ax.grid(True)
+#    plt.ylim((0,1050))
+#    ax.legend(["1","2","3","4"])
+#    plt.ylabel('Light intensity')
+#    
+#    ax = fig.add_subplot(312)        
+#    ax.plot(time_raw,MagAngle_raw)
+#    ax.grid(True)
+#    plt.ylim((0,360))
+#    plt.ylabel('Compass angle [Deg]')
+#    
+#    ax = fig.add_subplot(313)        
+#    ax.plot(time_raw,BattVoltage_raw)
+#    ax.grid(True)
+#    plt.ylim((5,10))
+#    plt.ylabel('Battery voltage [V]')
+#    plt.xlabel('Time [ms]')
+#    
+#    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
     #mySaveFig(plt,LDR_DF_PATH + "png/", "Nlights:" + str(Nlights) + " " + LDR_data["beginDate"] + "_1.png")
     #plt.show()
     
     
     # Polar plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111,polar=True)
-    LDR_real_pos = np.array(LDR_data["LDR_real_pos"])
-    for i in range(len(LDR_real_pos)):
-        # NOTE: here we are incorporating the knowledge of the orientation of the sensors
-        offset = float(LDR_real_pos[i])
-        ax.plot(np.radians(MagAngle_raw+offset), LDR_raw[:,i], marker='.', markersize=2, ls='', label=str(i+1))
-    
-    for light in LDR_data["lights"]:
-        ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
-    #plt.ylim((0,2*1050))
-    ax.set_rmax(2*1050)
-    ax.set_theta_direction('clockwise')
-    ax.set_theta_zero_location('N')
-    ax.legend()
-    ax.grid(True)
-    
-    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111,polar=True)
+#    LDR_real_pos = np.array(LDR_data["LDR_real_pos"])
+#    for i in range(len(LDR_real_pos)):
+#        # NOTE: here we are incorporating the knowledge of the orientation of the sensors
+#        offset = float(LDR_real_pos[i])
+#        ax.plot(np.radians(MagAngle_raw+offset), LDR_raw[:,i], marker='.', markersize=2, ls='', label=str(i+1))
+#    
+#    for light in LDR_data["lights"]:
+#        ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
+#    #plt.ylim((0,2*1050))
+#    ax.set_rmax(2*1050)
+#    ax.set_theta_direction('clockwise')
+#    ax.set_theta_zero_location('N')
+#    ax.legend()
+#    ax.grid(True)
+#    
+#    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
     #mySaveFig(plt,LDR_DF_PATH + "png/","Nlights:" + str(Nlights) + " " + LDR_data["beginDate"] + "_2.png")
     #plt.show()
     
     
-    
-    # Polar plot (fitted curve + points where derivate == 0)
-    fig = plt.figure()
-    ax = fig.add_subplot(111,polar=True)
-    
+    # Merges the data from all the sensors as a datapoint cloud
     LDR_real_pos = np.array(LDR_data["LDR_real_pos"])
     LDR_raw_all = np.array([])
     LDR_raw_angle_all = np.array([])
@@ -141,79 +146,86 @@ for LDR_DF in getFilesInDir(LDR_DF_PATH):
         # NOTE: here we are incorporating the knowledge of the orientation of the sensors
         offset = float(LDR_real_pos[i])
         
-        angle = np.radians(MagAngle_raw+offset)
-        angle = np.mod(angle, 2*np.pi);
+        angle = MagAngle_raw+offset
+        angle = np.mod(angle, 360.);
         magnitude = LDR_raw[:,i]
         LDR_raw_all = np.hstack([LDR_raw_all,LDR_raw[:,i]])
         LDR_raw_angle_all = np.hstack([LDR_raw_angle_all,angle])
     
-    #indices = np.argsort(LDR_raw_angle_all,axis=None)
-    #LDR_raw_angle_all = LDR_raw_angle_all[indices]
-    #LDR_raw_all = LDR_raw_all[indices]
     
-    #fit_pol = scipy.interpolate.splrep(LDR_raw_angle_all, LDR_raw_all, per=1)
-    fit_pol = np.poly1d(np.polyfit(LDR_raw_angle_all, LDR_raw_all, 6))
-    fit_angle = np.linspace(LDR_raw_angle_all.min(), LDR_raw_angle_all.max(), 800)
-    #fit_angle = np.linspace(0, 2*np.pi, 800)
-    fit_magnitude = fit_pol(fit_angle)
-    ax.plot(fit_angle, fit_magnitude, label="Fitted curve")
-    ax.plot(LDR_raw_angle_all, LDR_raw_all, marker='.', markersize=0.5, ls='', label="Datapoints")
+    # Discretizes the measurements as a linear plot
+    Nsamples = 50
+    LDR_curve = discretizePolar(LDR_raw_angle_all, LDR_raw_all, Nsamples)
+    LDR_angle = np.arange(0., 360., 360./float(Nsamples))
+    print("Measurement:")
+    print("LDR_curve="+str(LDR_curve.tolist()))
+    print("LDR_angle="+str(LDR_angle.tolist()))
     
-    fit_pol_der = np.polyder(fit_pol)
-    fit_magnitude_der = fit_pol_der(fit_angle)
-    fit_magnitude_der_min = abs(np.min(fit_magnitude_der))
-    fit_magnitude_der += fit_magnitude_der_min
+#    if Nlights >= 1:
+#        LDR_curve -= Light1_LDR_curve
+#    if Nlights >= 2:
+#        LDR_curve -= Light2_LDR_curve
+#    if Nlights >= 3:
+#        LDR_curve -= Light3_LDR_curve
+#    if Nlights >= 4:
+#        LDR_curve -= Light4_LDR_curve
     
-    # Find points with derivate == 0
-    all_roots = np.roots(fit_pol_der)
+    print("Removed offset:")
+    print("LDR_curve="+str(LDR_curve.tolist()))
+    print("LDR_angle="+str(LDR_angle.tolist()))
     
-    # Remove complex roots
-    indices = np.where(np.imag(all_roots)==0)
-    roots = np.real(all_roots[indices])
+    # Polar plot (fitted curve + points where derivate == 0)
+    fig = plt.figure(LDR_data["beginDate"] + " " + LDR_data["description"])
+    ax = fig.add_subplot(111,polar=True)
     
-    # Remove roots outside [0,2pi)
-    indices = (roots >= 0) & (roots < 2*np.pi)
-    roots = roots[indices]
-    print("Roots: "+str(roots))
+    plotClosedLine(ax, np.radians(LDR_angle), LDR_curve, "Curve")
+    ax.plot(np.radians(LDR_raw_angle_all), LDR_raw_all, marker='.', markersize=0.5, ls='', label="Datapoints")
     
-    ax.plot(roots, fit_pol(roots), marker='x', markersize=10, ls='', label="Roots")
-    
+    LDR_result_curve = np.zeros(Nsamples)
     for light in LDR_data["lights"]:
         ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
+        if isSameAngle(Light1_angle, light["angle"]):
+            print("Light 1")
+            intens_front = eval_intensity(Light1_LDR_curve, light["angle"])
+            intens_back = eval_intensity(Light1_LDR_curve, 180.+light["angle"])
+        elif isSameAngle(Light2_angle, light["angle"]):
+            print("Light 2")
+            intens_front = eval_intensity(Light2_LDR_curve, light["angle"])
+            intens_back = eval_intensity(Light2_LDR_curve, 180.+light["angle"])
+        elif isSameAngle(Light3_angle, light["angle"]):
+            print("Light 3")
+            intens_front = eval_intensity(Light3_LDR_curve, light["angle"])
+            intens_back = eval_intensity(Light3_LDR_curve, 180.+light["angle"])
+        elif isSameAngle(Light4_angle, light["angle"]):
+            print("Light 4")
+            intens_front = eval_intensity(Light4_LDR_curve, light["angle"])
+            intens_back = eval_intensity(Light4_LDR_curve, 180.+light["angle"])
+        intens_back = eval_intensity(Light1_LDR_curve, 180.+Light1_angle)
+        if intens_back < 0:
+            intens_back = 0
+        intens_back /= float(Nlights)
+        if light["intensity"] == 60:
+            intens = 1.*(intens_front - intens_back)*((light["distance"]/100.)**1.)
+        else:
+            intens = 1.*(intens_front - intens_back)*((light["distance"]/100.)**1.)
+        #intens_back += 250./float(Nlights)
+        print("Back="+str(intens_back))
+        print("Front="+str(intens))
+        (LDR_model_angle, LDR_model_curve) = lightModelCurve(Nsamples, angle=light["angle"], distance=light["distance"]/100., intensity=intens, ambient=intens_back)
+        #plotClosedLine(ax, np.radians(LDR_angle), LDR_model_curve)
+        LDR_result_curve += LDR_model_curve
+    
+    plotClosedLine(ax, np.radians(LDR_angle), LDR_result_curve, "Model")
+    
     #plt.ylim((0,2*1050))
     ax.set_rmax(2*1050)
     ax.set_theta_direction('clockwise')
     ax.set_theta_zero_location('N')
-    #ax.legend()
-    ax.grid(True)
-    
-    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
-    #mySaveFig(plt,LDR_DF_PATH + "png/","Nlights:" + str(Nlights) + " " + LDR_data["beginDate"] + "_2.png")
-    #plt.show()
-    
-    
-    
-    # Linear plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    
-    ax.plot(LDR_raw_angle_all, LDR_raw_all, marker='.', markersize=0.5, ls='', label="Datapoints")
-    ax.plot(fit_angle, fit_magnitude, label="Fitted curve")
-    
-    ax.plot(fit_angle, fit_magnitude_der, label="Fitted curve (diff)")
-    ax.plot(fit_angle, fit_magnitude_der_min + 0*fit_magnitude_der)
-    
-    ax.plot(roots, fit_pol(roots), marker='x', markersize=10, ls='', label="Roots")
-    
-    for light in LDR_data["lights"]:
-        ax.plot(np.radians(light["angle"]), 1024+1050.0*light["distance"]/150.0, marker='o', markersize=50.0*(light["intensity"]/100.0), ls='')
-    plt.ylim((0,2*1050))
-    
     ax.legend()
     ax.grid(True)
     
-    plt.suptitle(LDR_data["beginDate"] + " " + LDR_data["description"])
+    plt.suptitle("Intensity [0,1023]")
     #mySaveFig(plt,LDR_DF_PATH + "png/","Nlights:" + str(Nlights) + " " + LDR_data["beginDate"] + "_2.png")
     plt.show()
-    #exit()
+    
 
--- Software/helper.py
+++ Software/helper.py
@@ -9,6 +9,8 @@ import datetime
 import os
 import sys
 
+import numpy as np
+
 # Misc functions:
 
 # From http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
@@ -68,32 +70,82 @@ def getValue(description,default):
     return validType(string)
 
 
-
-def pltShowNonBlocking():
-	#plt.ion() # Enable real-time plotting to avoid blocking behaviour for plt.show()
-	plt.draw()
-	#plt.ioff() # Disable real-time plotting
-
-def pltNewFig():
-	fig = plt.figure()
-	#plt.draw()
-	return fig
-
-def pltSetFig(fig):
-	plt.figure(fig.number)
-
-def pltRefresh(fig):
-	fig.canvas.draw()
-
-def pltShow():
-	#plt.ion() # IMPORTANT: Enable real-time plotting
-	plt.draw()
-	#plt.ioff()
-
 def mapVals(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def mySaveFig(plt,path,file):
     makeDirs(path)
     plt.savefig(path+file)
+
+
+def discretizePolar(angle, intensity, Nsamples, radians = False):
+    # Note: assumes angle = np.mod(angle, 2*np.pi);
+    if not radians:
+        angleInc = 360./float(Nsamples)
+    else:
+        angleInc = (2.*np.pi)/float(Nsamples)
+    result = np.zeros(Nsamples)
+    for i in range(Nsamples):
+        angleIni = i*angleInc - angleInc/2.
+        angleEnd = angleIni + angleInc
+        (indices,) = np.where((angle >= angleIni) & (angle <= angleEnd))
+        if len(indices) > 0:
+            result[i] = np.mean(intensity[indices])
+        else:
+            result[i] = -1
+    # Interpolate empty samples
+    (emptySamples,) = np.where(np.array(result) == -1)
+    for i in emptySamples:
+        prev_i = i
+        while result[prev_i] == -1:
+            prev_i -= 1
+        next_i = i
+        while result[next_i % Nsamples] == -1:
+            next_i += 1
+        dist = next_i - prev_i
+        t = float(i - prev_i)/float(dist)
+        result[i] = result[prev_i]*t + result[next_i % Nsamples]*(1.-t)
+    return result
+
+def plotClosedLine(ax, angs, vals, lab=None):
+    angs = np.append(angs,angs[0])
+    vals = np.append(vals,vals[0])
+    ax.plot(angs, vals, label=lab)
+
+# Linear interpolation of intermediate values
+def eval_intensity(vals, angle):
+    angle = np.mod(angle, 360.);
+    Nsamples = vals.size
+    inc = 360./float(Nsamples)
+    prev = np.floor(angle/inc)
+    next = np.ceil(angle/inc)
+    dist = next-prev
+    t = (angle - float(prev)*inc)/float(dist)
+    return vals[prev]*t + vals[next % Nsamples]*(1.-t)
+
+def lightModelCurve(Nsamples, angle=0, distance=1, intensity=1, ambient=0):
+    vals = np.zeros(Nsamples)
+    angs = np.arange(0., 360., 360./float(Nsamples))
+    angs -= angle
+    angs = np.mod(angs, 360.);
+    
+    ambient = float(ambient)
+    
+    for i in range(Nsamples):
+        perceivedIntensity = float(intensity)/(float(distance)**2.)
+        if angs[i] >= 0. and angs[i] < 90.:
+            t = np.cos(np.radians(angs[i]))
+            vals[i] = perceivedIntensity*t + ambient
+        elif angs[i] > 270. and angs[i] < 360.:
+            t = np.cos(np.radians(angs[i]))
+            vals[i] = perceivedIntensity*t + ambient
+        else:
+            vals[i] = ambient
+    angs += angle
+    angs = np.mod(angs, 360.);
+    return (angs,vals)
+
+# Be careful with this function
+def isSameAngle(ang1, ang2, margin=10.):
+    return ang1+margin > ang2 and ang1-margin < ang2
 
