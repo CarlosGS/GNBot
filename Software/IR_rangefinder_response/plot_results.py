bf3bd4d34715e38d76922e8d4537081016418141
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2015-06-10 01:37:07 +0100

    Linearized IR rangefinder response

--- Software/IR_rangefinder_response/plot_results.py
+++ Software/IR_rangefinder_response/plot_results.py
@@ -19,6 +19,8 @@ f, ax = subplots(1)
 
 data = loadFromFile("","distanceLog.p")
 
+print(data)
+
 
 
 distances = np.array(data['distances'])
@@ -28,10 +30,61 @@ measurementMax = np.array(data['measurementMax'])
 
 measurementMin = mapVals(measurementMin,0.,1023.,0.,5.)
 measurementMax = mapVals(measurementMax,0.,1023.,0.,5.)
+measurementAvg = (measurementMin+measurementMax)/2
 
 ax.plot(distances,measurementMax)
-ax.plot(distances,(measurementMin+measurementMax)/2)
+ax.plot(distances,measurementAvg)
 ax.plot(distances,measurementMin)
+
+
+# Fitting to exponential curve
+
+#x1 = 9.
+#y1 = float(measurementAvg[6])
+
+x1 = 12.
+y1 = float(measurementAvg[7])
+
+#x2 = 21.
+#y2 = float(measurementAvg[9])
+
+x2 = 37.
+y2 = float(measurementAvg[11])
+
+#x2 = 86.
+#y2 = float(measurementAvg[14])
+
+#x2 = 114.
+#y2 = float(measurementAvg[15])
+
+
+
+
+#D = x1
+#A = y1
+#B = np.e
+#C = 0.1
+
+print x1, y1, x2, y2
+
+K = x1*(y1-y2) / (1.-x1/x2)
+C = y2-K/x2
+fit = K*(1./distances)+C
+
+#K = 14.6358265503
+#C = -0.173084289376
+
+#a = (np.log(K)-np.log(C+y3))/np.log(x3)
+#K = (x1**a)*(y1-y2) / (1.-(x1**a)/(x2**a))
+#C = y2-K/(x2**a)
+#fit = K*(1./(distances**a))+C
+#print a, K, C
+
+#ax.plot(distances,fit)
+
+
+ax.plot(K/(measurementAvg-C),measurementAvg,"o")
+
 
 
 
@@ -45,10 +98,19 @@ ax.set_xlabel('Actual distance [cm]', fontsize=16)
 ax.set_title('IR range-finder response curve', fontsize=16)
 
 
-ax.legend(["max","avg","min"])
+ax.legend(["max","avg","min","fit"])
 
 tight_layout()
-savefig("IR_sensor_response_curve.pdf")
+
+
+
+
+f, ax = subplots(1)
+
+ax.plot(distances,K/(measurementAvg-C),"r")
+ax.plot(distances,distances,"b")
+
+#savefig("IR_sensor_response_curve.pdf")
 show()
 
 
