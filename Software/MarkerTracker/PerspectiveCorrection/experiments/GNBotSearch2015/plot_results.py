88a202e0bc9aff7a2b617304df3c64561ea29311
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2015-08-12 17:26:23 +0200

    Post-processing of motion data, to segment each robot's trajectory

--- Software/MarkerTracker/PerspectiveCorrection/experiments/GNBotSearch2015/plot_results.py
+++ Software/MarkerTracker/PerspectiveCorrection/experiments/GNBotSearch2015/plot_results.py
@@ -16,7 +16,7 @@ rc('text',usetex=True)
 rc('font',**{'family':'serif','serif':['Computer Modern']})
 
 
-data_file_name = "2015-08-07-183501"
+data_file_name = "2015-08-07-175946"
 data = loadFromFile("",data_file_name+"_posLog_raw.p")
 
 ts = data['videoTimestamp']
@@ -24,12 +24,50 @@ posX = data['posX']
 posY = data['posY']
 
 
+# Segment position data into N robot paths
+N_markers = len(posX[0,:])
+paths = {}
+paths['timestamp'] = ts
+for r in xrange(N_markers):
+    paths[r] = []
+
+indices = range(len(ts))
+indices.reverse()
+first = True
+for i in indices:
+    if ts[i] < 20: # Skip first 20 seconds
+        break
+    blobs = [np.array([posX[i,r],posY[i,r]]) for r in xrange(N_markers)]
+    for r in xrange(N_markers):
+        pos = []
+        if first:
+            pos = blobs[r]
+        else:
+            last_pos = np.array(paths[r][-1])
+            distances = [np.linalg.norm(blobs[ri]-last_pos) for ri in xrange(N_markers)]
+            idmin = np.argmin(distances)
+            minDist = distances[idmin]
+            if minDist < 10:
+                pos = blobs[idmin]
+            else:
+                pos = last_pos
+        paths[r].append(pos.tolist())
+    first = False
+
+for r in xrange(N_markers):
+    path = np.array(paths[r])
+    paths[r] = np.flipud(path)
+
+
 map_points = loadFromFile("",data_file_name+"map_data.p")
 map_points = np.vstack((map_points,map_points[0,:])) # Convert to closed curve
 
 figure()
 plot(map_points[:,0], map_points[:,1], 'b')
-plot(posX, posY,'.g', ms=0.1)
+plot(posX,posY,'.y', ms=0.5)
+for r in xrange(N_markers):
+    plot(paths[r][:,0],paths[r][:,1],linewidth=0.5)
+    #plot(paths[r][:,0],paths[r][:,1],'.', ms=0.1)
 xlabel('Position in X [cm]', fontsize=16)
 ylabel('Position in Y [cm]', fontsize=16)
 axis('equal')
@@ -38,6 +76,7 @@ ylim([-410,10])
 #tight_layout()
 savefig(data_file_name+"position_log.png")
 
+saveToFile(paths,"",data_file_name+"_posLog.p")
 
 #figure()
 #plot(ts, pos[:,0],'b')
