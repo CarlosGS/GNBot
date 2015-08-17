486a73d41c44338f257518c81455ce7964e307a8
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2015-08-17 22:29:42 +0200

    Improved visualization of IR rangefinder response

--- Software/IR_rangefinder_response/initialMeasurement/plot_results.py
+++ Software/IR_rangefinder_response/initialMeasurement/plot_results.py
@@ -15,7 +15,7 @@ rc('text',usetex=True)
 # Change all fonts to 'Computer Modern'
 rc('font',**{'family':'serif','serif':['Computer Modern']})
 
-f, ax = subplots(1)
+f, ax = subplots(1,figsize=(11,5))
 
 data = loadFromFile("","distanceLog.p")
 
@@ -28,14 +28,37 @@ measurementMin = np.array(data['measurementMin'])
 measurementMax = np.array(data['measurementMax'])
 
 
-#measurementMin = mapVals(measurementMin,0.,1023.,0.,5.)
-#measurementMax = mapVals(measurementMax,0.,1023.,0.,5.)
+measurementMin = mapVals(measurementMin,0.,1023.,0.,5.)
+measurementMax = mapVals(measurementMax,0.,1023.,0.,5.)
 measurementAvg = (measurementMin+measurementMax)/2
 
 ax.plot(distances,measurementMax)
 ax.plot(distances,measurementAvg)
 ax.plot(distances,measurementMin)
 
+#ax.plot([0,200],[0.7,0.7],'k--')
+ax.plot([80,80],[0,3.5],'k--',linewidth=2)
+ax.plot([40,40],[0,3.5],'k--',linewidth=2)
+
+
+
+
+
+ax.arrow(40, 2.2, -22.5, 0, head_width=0.1, head_length=5, fc='k', ec='k')
+ax.text(40-21, 2-0.2, 'Maximum\nsensitivity', fontsize=16)
+ax.text(40+2.5, 3, '$d=40cm$', fontsize=14)
+
+
+ax.arrow(80, 1.4, -27.5, 0, head_width=0.1, head_length=5, fc='k', ec='k')
+ax.text(85-31, 1.2, 'Manuf. spec.', fontsize=16)
+ax.text(80+2.5, 3, '$d=80cm$', fontsize=14)
+
+
+ax.arrow(80, 0.8, 50, 0, head_width=0.1, head_length=5, fc='k', ec='k')
+ax.text(85, 0.6, 'Outside manuf. spec.', fontsize=16)
+
+
+ax.add_patch(Rectangle((0,0),7,3.5,linewidth=0,facecolor=(1,0,0,0.3)))
 
 # Fitting to exponential curve
 
@@ -85,7 +108,7 @@ print("C="+str(C))
 #ax.plot(distances,fit)
 
 
-ax.plot(K/(measurementAvg-C),measurementAvg,"o")
+#ax.plot(K/(measurementAvg-C),measurementAvg,"o")
 
 
 
@@ -97,22 +120,25 @@ ax.set_xlim([0,200])
 ax.set_ylabel('Sensor output [V]', fontsize=16)
 ax.set_xlabel('Actual distance [cm]', fontsize=16)
 
-ax.set_title('IR range-finder response curve', fontsize=16)
+ax.set_title('IR rangefinder response curve (I)', fontsize=18)
 
 
-ax.legend(["max","avg","min","fit"])
+ax.legend(["$Maximum$","$Average$","$Minimum$"])#,"fit"])
 
 tight_layout()
 
+savefig("IR_sensor_response_curve.pdf")
+savefig("IR_sensor_response_curve.png")
 
-
+show()
+exit()
 
 f, ax = subplots(1)
 
 ax.plot(distances,K/(measurementAvg-C),"r")
 ax.plot(distances,distances,"b")
 
-#savefig("IR_sensor_response_curve.pdf")
+
 show()
 
 
