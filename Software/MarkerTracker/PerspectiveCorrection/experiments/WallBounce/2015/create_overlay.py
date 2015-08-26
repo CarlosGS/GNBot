4fc7243a34508fb0091a1c5f797f1114d1b37e20
Carlos Garcia Saura <carlos.garcia-saura14@imperial.ac.uk>
2015-08-26 15:06:11 +0100

    Work on the wallbounce experiment overlay

---
+++ Software/MarkerTracker/PerspectiveCorrection/experiments/WallBounce/2015/create_overlay.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from helper import *
+import cv2
+import numpy as np
+
+
+
+data_file_name = "2015-08-07-183814"
+paths = loadFromFile("",data_file_name+"_posLog.p")
+
+
+ts = paths['timestamp']
+N_markers = len(paths.keys())-1
+N_points = len(paths[0][:,0])
+
+# Low pass filtering:
+for r in xrange(N_markers):
+    first = True
+    for i in xrange(N_points):
+        if not first:
+            paths[r][i,:] = paths[r][i-1,:]*0.9 + paths[r][i,:]*0.1
+        else:
+            first = False
+
+
+
+
+data = loadFromFile("./","perspective_correction_data.p")
+perspective_correction_matrix = data['perspective_correction_matrix']
+warpSize = data['warpSize']
+cm_per_pixel = data['cm_per_pixel']
+
+src_points = np.array(data['src_points'],np.float32)
+dst_points = np.array(data['dst_points'],np.float32)
+
+inv_perspective_correction_matrix = cv2.getPerspectiveTransform(dst_points,src_points)
+
+print inv_perspective_correction_matrix
+
+# Apply inverse transform to every point
+for r in xrange(N_markers):
+    for i in xrange(N_points):
+        src = np.array([[paths[r][i,:]]],np.float32)
+        dst = cv2.perspectiveTransform(src,inv_perspective_correction_matrix)
+        while len(dst.shape) > 1:
+            dst = dst[0]
+        paths[r][i,:] = dst
+
+
+img = cv.imread("Screenshot from 2015-08-07-183814.webm.png")
+
+#colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,255),(0,0,0)]
+colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,255),(0,0,0)]
+
+def tuple_lerp(start,end,l):
+    start = np.array(start)
+    end = np.array(end)
+    res = start*(1-l) + end*l
+    return tuple(res)
+
+
+for r in xrange(N_markers):
+    curr = paths[r][0,:]
+    prev = (int(curr[0]),int(curr[1]))
+    for i in xrange(N_points):
+        progress = float(i)/float(N_points)
+        curr = paths[r][i,:]
+        new = (int(curr[0]),int(curr[1]))
+        #cv2.line(img, prev, new, tuple_lerp((128,128,128),colors[r],progress))
+        col = (colors[r][0],colors[r][1],colors[r][2],int(progress*255))
+        cv2.line(img, prev, new, col)
+        prev = new
+
+
+
+cv2.imwrite("overlay.png",img)
+
+
